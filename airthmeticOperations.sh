3cc6aa6d113dc815c8317f72b4d7fc1e4a7b04fb
hks9211 <honeykrsingh16@gmail.com>
2020-01-08 18:17:46 +0530

    reading values done

--- airthmeticOperations.sh
+++ airthmeticOperations.sh
@@ -1,2 +1,7 @@
 #### Welcome to Airthmetic computation problem ####
+#!/bash/bin -x
 
+#reading values from users
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "ENter a value for c: " c
