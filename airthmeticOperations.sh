64cba08fbaf9d689528981db5e6f841ae5df56f4
hks9211 <honeykrsingh16@gmail.com>
2020-01-08 18:27:00 +0530

    computation completed

--- airthmeticOperations.sh
+++ airthmeticOperations.sh
@@ -6,6 +6,7 @@ a=0;
 b=0;
 c=0;
 result1=0;
+result2=0;
 
 #reading values from users
 read -p "Enter a value for a: " a
@@ -13,4 +14,4 @@ read -p "Enter a value for b: " b
 read -p "ENter a value for c: " c
 
 result1=$(( $a + $b * $c ))
-
+result2=$(( $a * $b + $c ))
